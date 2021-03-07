5823db990219b638687c6fd7c5dbee1dbfe3ee11
Mouhamadoul Amine THIAM <thiambarathiam97@gmail.com>
2021-03-07 00:37:12 +0000

    cofiguration det  mise en place de swagger

--- src/main/java/com/multi/backend/BackEndApplication.java
+++ src/main/java/com/multi/backend/BackEndApplication.java
@@ -11,11 +11,27 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class BackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.apis(RequestHandlerSelectors.basePackage("com.multi"))
+			.paths(PathSelectors.any())
+			.build();
 	}
 
 	@Bean
