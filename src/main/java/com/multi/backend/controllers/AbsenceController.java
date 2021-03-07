62cdd4a74b904063628cdd3aec94a8b8bc3e2776
Mouhamadoul Amine THIAM <thiambarathiam97@gmail.com>
2021-03-07 19:27:11 +0000

    add cross header origin enabled

--- src/main/java/com/multi/backend/controllers/AbsenceController.java
+++ src/main/java/com/multi/backend/controllers/AbsenceController.java
@@ -16,8 +16,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/absences")
 public class AbsenceController {
--- src/main/java/com/multi/backend/controllers/EmployeController.java
+++ src/main/java/com/multi/backend/controllers/EmployeController.java
@@ -19,7 +19,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/employes")
 public class EmployeController {
