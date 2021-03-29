4d69b565b664022c73800324ca121af3bde64744
Mouhamadoul Amine THIAM <thiambarathiam97@gmail.com>
2021-03-29 01:11:38 +0000

    add https://multi-thread-front-end.web.app/ to the allowed origin cors

--- src/main/java/com/multi/backend/BackEndApplication.java
+++ src/main/java/com/multi/backend/BackEndApplication.java
@@ -41,7 +41,8 @@ public class BackEndApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration
+				.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://multi-thread-front-end.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
 				"Access-Control-Request-Headers"));
--- src/main/java/com/multi/backend/security/ApplicationSecurityConfig.java
+++ src/main/java/com/multi/backend/security/ApplicationSecurityConfig.java
@@ -77,7 +77,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration
+                .setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://multi-thread-front-end.web.app/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
                 "Access-Control-Request-Headers"));
