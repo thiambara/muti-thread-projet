2d605f8f4dcbfa7741835a8d3520948d332e37f8
thiambara <51300616+thiambara@users.noreply.github.com>
2021-05-29 11:28:10 +0000

    Update BackEndApplication.java

--- src/main/java/com/multi/backend/BackEndApplication.java
+++ src/main/java/com/multi/backend/BackEndApplication.java
@@ -28,7 +28,7 @@ public class BackEndApplication {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.multi"))
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
 	}
 
