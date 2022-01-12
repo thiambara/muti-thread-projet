a1c445bea0b58ea8beb046a4420789dadfffeacb
thiambara <51300616+thiambara@users.noreply.github.com>
2022-01-12 09:24:10 +0000

    Update ApplicationSecurityConfig.java

--- src/main/java/com/multi/backend/security/ApplicationSecurityConfig.java
+++ src/main/java/com/multi/backend/security/ApplicationSecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
+
 
 import java.util.Arrays;
 
